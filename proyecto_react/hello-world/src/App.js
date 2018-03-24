ab5caee488c8b66bee5c71c36e4ed764b88ebb34
Daniel Contreras <dcontrer@pros.com>
2018-03-23 17:34:34 -0700

    Creado proyecto invie react

--- proyecto_react/hello-world/src/App.js
+++ proyecto_react/hello-world/src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logoPlatzi from "./platzi.webp"
 import './App.css';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class App extends Component {
-  render() {
+  state = {
+    logo: logo
+  }
+  activeAnimation() 
+  {
+    this.setState({
+      logo: logoPlatzi
+    })
+  }
+
+
+  render() 
+  {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <CSSTransitionGroup transitionName="fade"
+        transitionEnterTimeOut={1000} 
+        transitionLeaveTimeOut={1000}>
+          <img key={this.state.logo}
+          src={this.state.logo} 
+          className="App-logo" 
+          alt="logo" />
+        </CSSTransitionGroup>
           <h1 className="App-title">Hola mundo!!!</h1>
+          <button onClick={this.activeAnimation.bind(this)}>Click me!</button>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ proyecto_react/invie/src/Invie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Invie.css';
+
+class Invie extends Component {
+  render() {
+    return (
+      <section className="Invie">
+        {/*Portada */}
+        {/* Guitarras */}
+        {/* Footer */}
+      </section>
+    );
+  }
+}
+
+export default Invie;
